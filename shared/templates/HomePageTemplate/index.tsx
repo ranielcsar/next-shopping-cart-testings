ba939e5803e30efca680766f6f1c808e110c3a0c
ranoob <whyfck@gmail.com>
2022-12-28 19:58:06 -0300

    fixes: item price and page title

--- shared/templates/HomePageTemplate/index.tsx
+++ shared/templates/HomePageTemplate/index.tsx
@@ -2,18 +2,22 @@ import { ProductList } from '@/components'
 import { T_Product } from '@/types'
 
 export function HomePageTemplate() {
-  return <ProductList products={products} />
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  )
 }
 
 const products: T_Product[] = Array.from({ length: 15 }, (_, index: number) => {
   const customIndex = index + 1
   const randomId = Math.floor(Math.random() * 1000)
-  const randomPrice = Math.floor(Math.random() * 250)
+  const randomPrice = 1000
 
   return {
     id: index,
     name: `Produto ${customIndex}`,
-    price: randomPrice,
+    price: randomPrice / customIndex,
     imageUrl: `https://picsum.photos/id/${randomId}/600`
   }
 })
