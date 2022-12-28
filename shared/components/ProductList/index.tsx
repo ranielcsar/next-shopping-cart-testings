dfffc881c8ffafec0c8a519de2337b81e1850d1a
ranoob <whyfck@gmail.com>
2022-12-28 19:57:35 -0300

    feat: added func to add cart

--- shared/components/ProductList/index.tsx
+++ shared/components/ProductList/index.tsx
@@ -1,14 +1,26 @@
 import styles from './styles.module.css'
 import { ProductCard } from '@/components'
+import { useShop } from '@/hooks'
+import { ActionsEnum } from '@/reducers/shop-reducer'
 import { T_Product } from '@/types'
 
 const { container } = styles
 
 export function ProductList({ products }: { products: T_Product[] }) {
+  const { dispatch } = useShop()
+
+  const handleOnAddToCart = (product: T_Product) => {
+    dispatch({ type: ActionsEnum.ADD, payload: { ...product, quantity: 1 } })
+  }
+
   return (
     <section className={container}>
       {products.map((product) => (
-        <ProductCard product={product} />
+        <ProductCard
+          product={product}
+          key={product.name}
+          onAddClick={() => handleOnAddToCart(product)}
+        />
       ))}
     </section>
   )
