6a571517711d0723b6d98660c41a9041ab1a094e
ranoob <whyfck@gmail.com>
2023-01-04 20:38:38 -0300

    feat: added increment/decrement item quantity and remove from cart actions

--- shared/reducers/shop-reducer/index.tsx
+++ shared/reducers/shop-reducer/index.tsx
@@ -1,9 +1,15 @@
-import { removeFromCart } from './utils'
+import {
+  removeFromCart,
+  incrementItemQuantity,
+  decrementItemQuantity
+} from './utils'
 import { ShopActionProps, ShopStateProps } from '@/contexts'
 
 export enum ShopActions {
   'ADD_TO_CART' = 'ADD_TO_CART',
-  'REMOVE_FROM_CART' = 'REMOVE_FROM_CART'
+  'REMOVE_FROM_CART' = 'REMOVE_FROM_CART',
+  'INCREMENT_ITEM_QUANTITY' = 'INCREMENT_ITEM_QUANTITY',
+  'DECREMENT_ITEM_QUANTITY' = 'DECREMENT_ITEM_QUANTITY'
 }
 
 export function shopReducer(state: ShopStateProps, action: ShopActionProps) {
@@ -17,6 +23,16 @@ export function shopReducer(state: ShopStateProps, action: ShopActionProps) {
       return {
         ...state,
         cart: removeFromCart(state.cart, action.payload.id)
+      }
+    case ShopActions.INCREMENT_ITEM_QUANTITY:
+      return {
+        ...state,
+        cart: incrementItemQuantity(state.cart, action.payload.id)
+      }
+    case ShopActions.DECREMENT_ITEM_QUANTITY:
+      return {
+        ...state,
+        cart: decrementItemQuantity(state.cart, action.payload.id)
       }
     default:
       return state
