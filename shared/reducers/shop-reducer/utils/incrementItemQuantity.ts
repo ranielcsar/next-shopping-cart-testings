4f98b69a1ce558aab90b927083400686cb5c479d
ranoob <whyfck@gmail.com>
2023-01-04 20:39:27 -0300

    feat: increment quantity function

---
+++ shared/reducers/shop-reducer/utils/incrementItemQuantity.ts
@@ -0,0 +1,21 @@
+import { ShopStateProps, T_ProductID } from '@/contexts'
+
+export function incrementItemQuantity(
+  cart: ShopStateProps['cart'],
+  id: T_ProductID
+) {
+  const cartCopy = cart.slice()
+
+  const newCart = cartCopy.map((product) => {
+    if (product.id === id) {
+      return {
+        ...product,
+        quantity: product.quantity + 1
+      }
+    }
+
+    return product
+  })
+
+  return newCart
+}
