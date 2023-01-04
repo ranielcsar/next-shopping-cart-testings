8b3a3f88738f7c5b28ddba6602d69f3779453fd8
ranoob <whyfck@gmail.com>
2023-01-04 20:39:38 -0300

    feat: decrement quantity function

---
+++ shared/reducers/shop-reducer/utils/decrementItemQuantity.ts
@@ -0,0 +1,25 @@
+import { ShopStateProps, T_ProductID } from '@/contexts'
+
+export function decrementItemQuantity(
+  cart: ShopStateProps['cart'],
+  id: T_ProductID
+) {
+  const cartCopy = cart.slice()
+
+  const newCart = cartCopy.map((product) => {
+    if (product.id === id) {
+      const newQuantity = product.quantity - 1
+
+      if (newQuantity === 0) return
+
+      return {
+        ...product,
+        quantity: newQuantity
+      }
+    }
+
+    return product
+  })
+
+  return newCart
+}
