1f13865bafbc26031f89fc6efa1f18738cb668e0
ranoob <whyfck@gmail.com>
2023-01-04 14:13:08 -0300

    changes: change the shop hook name

--- shared/templates/HomePageTemplate/index.tsx
+++ shared/templates/HomePageTemplate/index.tsx
@@ -1,23 +1,8 @@
 import { ProductList } from '@/components'
-import { T_Product } from '@/types'
+import { useShop } from '@/hooks'
 
 export function HomePageTemplate() {
-  return (
-    <>
-      <ProductList products={products} />
-    </>
-  )
+  const { products } = useShop()
+
+  return <ProductList products={products} />
 }
-
-const products: T_Product[] = Array.from({ length: 15 }, (_, index: number) => {
-  const customIndex = index + 1
-  const randomId = Math.floor(Math.random() * 1000)
-  const randomPrice = 1000
-
-  return {
-    id: index,
-    name: `Produto ${customIndex}`,
-    price: randomPrice / customIndex,
-    imageUrl: `https://picsum.photos/id/${randomId}/600`
-  }
-})
