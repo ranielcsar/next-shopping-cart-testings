d1cf4dd0a32b7f2959a22cad20bef5f9c9891bf2
ranoob <whyfck@gmail.com>
2023-01-04 17:47:12 -0300

    change: remove from shop folder

---
+++ __tests__/shopReducerTest.tsx
@@ -0,0 +1,64 @@
+import { initialShopState } from '@/contexts'
+import { shopReducer } from '@/reducers'
+import { ShopActions } from '@/reducers/shop-reducer'
+import { T_Product } from '@/types'
+
+const product: T_Product = {
+  id: 0,
+  name: '',
+  price: 0,
+  imageUrl: ''
+}
+
+function addToCartTest() {
+  const payload = {
+    ...product,
+    quantity: 1
+  }
+
+  const action = {
+    type: ShopActions.ADD_TO_CART,
+    payload
+  }
+
+  const updatedState = shopReducer(initialShopState, action)
+
+  expect(updatedState).toEqual({
+    ...initialShopState,
+    cart: [payload]
+  })
+}
+
+function removeFromCartTest() {
+  const state = {
+    ...initialShopState,
+    cart: [
+      {
+        ...product,
+        quantity: 1
+      }
+    ]
+  }
+
+  const payload = {
+    id: product.id
+  }
+
+  const action = {
+    type: ShopActions.REMOVE_FROM_CART,
+    payload
+  }
+
+  const updatedState = shopReducer(state, action)
+
+  expect(updatedState).toEqual({
+    ...initialShopState,
+    cart: []
+  })
+}
+
+describe('Shop reducer', () => {
+  it('should update cart in state when add to cart', addToCartTest)
+
+  it('should update cart in state when remove from cart', removeFromCartTest)
+})
