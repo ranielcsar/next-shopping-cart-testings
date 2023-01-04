5e1437c566d71815439a3cf1dff55ac9ce0aa025
ranoob <whyfck@gmail.com>
2023-01-04 14:12:57 -0300

    change: change action name to ADD_TO_CART

--- shared/reducers/shop-reducer/index.tsx
+++ shared/reducers/shop-reducer/index.tsx
@@ -1,12 +1,12 @@
 import { ShopActionProps, ShopStateProps } from '@/contexts'
 
-export enum ActionsEnum {
-  'ADD' = 'ADD'
+export enum ShopActions {
+  'ADD_TO_CART' = 'ADD_TO_CART'
 }
 
 export function shopReducer(state: ShopStateProps, action: ShopActionProps) {
   switch (action.type) {
-    case ActionsEnum.ADD:
+    case ShopActions.ADD_TO_CART:
       return {
         ...state,
         cart: [...state.cart, action.payload]
