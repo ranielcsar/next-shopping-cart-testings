a3faf9fca3839bfabee48b089eb72f1505dead96
ranoob <whyfck@gmail.com>
2023-01-04 17:53:25 -0300

    feat: added action to remove product from cart

--- shared/reducers/shop-reducer/index.tsx
+++ shared/reducers/shop-reducer/index.tsx
@@ -1,7 +1,9 @@
+import { removeFromCart } from './utils'
 import { ShopActionProps, ShopStateProps } from '@/contexts'
 
 export enum ShopActions {
-  'ADD_TO_CART' = 'ADD_TO_CART'
+  'ADD_TO_CART' = 'ADD_TO_CART',
+  'REMOVE_FROM_CART' = 'REMOVE_FROM_CART'
 }
 
 export function shopReducer(state: ShopStateProps, action: ShopActionProps) {
@@ -10,6 +12,11 @@ export function shopReducer(state: ShopStateProps, action: ShopActionProps) {
       return {
         ...state,
         cart: [...state.cart, action.payload]
+      }
+    case ShopActions.REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: removeFromCart(state.cart, action.payload.id)
       }
     default:
       return state
