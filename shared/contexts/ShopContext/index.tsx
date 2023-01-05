8b8748f52145b1b1ccf2944c59258f5d7a74b5fb
ranoob <whyfck@gmail.com>
2023-01-05 16:26:45 -0300

    changes: delete product on cart type due to irrevelance

--- shared/contexts/ShopContext/index.tsx
+++ shared/contexts/ShopContext/index.tsx
@@ -1,22 +1,17 @@
-import { shopReducer } from '@/reducers'
-import { ShopActions } from '@/reducers/shop-reducer'
+import { shopReducer, ShopActions } from '@/reducers'
 import { T_Product } from '@/types'
 import { createContext, Dispatch, PropsWithChildren, useReducer } from 'react'
 
 export type T_ProductID = T_Product['id']
 
-export type T_ProductOnCart = {
-  quantity: number
-} & T_Product
-
 export type ShopStateProps = {
-  cart: T_ProductOnCart[]
+  cart: T_Product[]
   products: T_Product[]
 }
 
 export type ShopActionProps = {
   type: ShopActions
-  payload: T_ProductOnCart
+  payload: T_Product
 }
 
 export type ShopContextProps = {
@@ -33,12 +28,13 @@ const products: T_Product[] = Array.from({ length: 15 }, (_, index: number) => {
     id: index,
     name: `Produto ${customIndex}`,
     price: randomPrice / customIndex,
-    imageUrl: `https://picsum.photos/id/${randomId}/600`
+    imageUrl: `https://picsum.photos/id/${randomId}/600`,
+    quantity: 0
   }
 })
 
-export const initialShopState = {
-  cart: [] as unknown as T_ProductOnCart[],
+export const initialShopState: ShopStateProps = {
+  cart: [] as T_Product[],
   products
 }
 
