b5c5c9224030c035ae1fc2f3c06b0339225f4aa0
ranoob <whyfck@gmail.com>
2023-01-05 17:50:26 -0300

    changes: move handles to father component

--- shared/components/ProductList/index.tsx
+++ shared/components/ProductList/index.tsx
@@ -1,28 +1,22 @@
 import styles from './styles.module.css'
 import { ProductCard } from '@/components'
-import { useShop } from '@/hooks'
-import { ShopActions } from '@/reducers/shop-reducer'
 import { T_Product } from '@/types'
 
 const { container } = styles
 
-export function ProductList({ products }: { products: T_Product[] }) {
-  const { dispatch } = useShop()
+export type ProductListProps = {
+  products: T_Product[]
+  handleOnAddToCart: (product: T_Product) => () => void
+}
 
-  const handleOnAddToCart = (product: T_Product) => {
-    dispatch({
-      type: ShopActions.ADD_TO_CART,
-      payload: { ...product, quantity: 1 }
-    })
-  }
-
+export function ProductList({ products, handleOnAddToCart }: ProductListProps) {
   return (
     <section className={container}>
       {products.map((product) => (
         <ProductCard
           product={product}
           key={product.name}
-          onAddClick={() => handleOnAddToCart(product)}
+          onAddClick={handleOnAddToCart(product)}
         />
       ))}
     </section>
