37dc2ddda41040ac14245dc63646facfd4165151
ranoob <whyfck@gmail.com>
2023-01-05 17:45:51 -0300

    added cart list with handles

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,13 +1,33 @@
-import { ProductList } from '@/components'
+import { CartList } from '@/components'
 import { useShop } from '@/hooks'
+import { ShopActions } from '@/reducers'
 import { MainTemplate } from '@/templates'
+import { T_Product } from '@/types'
 
 export default function Cart() {
-  const { cart } = useShop()
+  const { cart, dispatch } = useShop()
+
+  const handleIncrementQuantity = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.INCREMENT_ITEM_QUANTITY,
+      payload: product
+    })
+  }
+
+  const handleDecrementQuantity = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.DECREMENT_ITEM_QUANTITY,
+      payload: product
+    })
+  }
 
   return (
     <MainTemplate title="Carrinho">
-      <ProductList products={cart} />
+      <CartList
+        cart={cart}
+        handleDecrementQuantity={handleDecrementQuantity}
+        handleIncrementQuantity={handleIncrementQuantity}
+      />
     </MainTemplate>
   )
 }
