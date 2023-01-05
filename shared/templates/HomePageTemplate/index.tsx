1f802eb4f52e3e175c5f4780cc79ac534b55e8d2
ranoob <whyfck@gmail.com>
2023-01-05 17:50:04 -0300

    feat: added handles

--- shared/templates/HomePageTemplate/index.tsx
+++ shared/templates/HomePageTemplate/index.tsx
@@ -1,8 +1,19 @@
 import { ProductList } from '@/components'
 import { useShop } from '@/hooks'
+import { ShopActions } from '@/reducers'
+import { T_Product } from '@/types'
 
 export function HomePageTemplate() {
-  const { products } = useShop()
+  const { products, dispatch } = useShop()
 
-  return <ProductList products={products} />
+  const handleOnAddToCart = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.ADD_TO_CART,
+      payload: { ...product, quantity: 1 }
+    })
+  }
+
+  return (
+    <ProductList products={products} handleOnAddToCart={handleOnAddToCart} />
+  )
 }
