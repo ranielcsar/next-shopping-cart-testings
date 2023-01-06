81e615f354001506c5591077545d4e5847b8c806
ranoob <whyfck@gmail.com>
2023-01-06 14:33:38 -0300

    changes: moved types to types folder

--- shared/contexts/ShopContext/index.tsx
+++ shared/contexts/ShopContext/index.tsx
@@ -1,23 +1,6 @@
-import { shopReducer, ShopActions } from '@/reducers'
-import { T_Product } from '@/types'
-import { createContext, Dispatch, PropsWithChildren, useReducer } from 'react'
-
-export type T_ProductID = T_Product['id']
-
-export type ShopStateProps = {
-  cart: T_Product[]
-  products: T_Product[]
-}
-
-export type ShopActionProps = {
-  type: ShopActions
-  payload: T_Product
-}
-
-export type ShopContextProps = {
-  state: ShopStateProps
-  dispatch: Dispatch<ShopActionProps>
-}
+import { shopReducer } from '@/reducers'
+import { ShopContextProps, ShopStateProps, T_Product } from '@/types'
+import { createContext, PropsWithChildren, useReducer } from 'react'
 
 const products: T_Product[] = Array.from({ length: 15 }, (_, index: number) => {
   const customIndex = index + 1
