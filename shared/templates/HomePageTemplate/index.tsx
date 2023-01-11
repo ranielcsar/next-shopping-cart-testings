d9c7bf7d9a1d7a9009f9599aefdba364177ba328
ranoob <whyfck@gmail.com>
2023-01-11 15:58:29 -0300

    changes: moved handles to ProductList

--- shared/templates/HomePageTemplate/index.tsx
+++ shared/templates/HomePageTemplate/index.tsx
@@ -1,19 +1,8 @@
 import { ProductList } from '@/components'
 import { useShop } from '@/hooks'
-import { ShopActions } from '@/reducers'
-import { T_Product } from '@/types'
 
 export function HomePageTemplate() {
-  const { products, dispatch } = useShop()
+  const { products } = useShop()
 
-  const handleOnAddToCart = (product: T_Product) => () => {
-    dispatch({
-      type: ShopActions.ADD_TO_CART,
-      payload: { ...product, quantity: 1 }
-    })
-  }
-
-  return (
-    <ProductList products={products} handleOnAddToCart={handleOnAddToCart} />
-  )
+  return <ProductList products={products} />
 }
