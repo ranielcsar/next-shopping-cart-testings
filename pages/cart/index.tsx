adf23b8c2dcb2bd06b974c65eae2e33dccc728c7
ranoob <whyfck@gmail.com>
2023-01-11 15:53:42 -0300

    move: moved handles to CartList

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,33 +1,13 @@
 import { CartList, TotalValueCard } from '@/components'
 import { useShop } from '@/hooks'
-import { ShopActions } from '@/reducers'
 import { MainTemplate } from '@/templates'
-import { T_Product } from '@/types'
 
 export default function Cart() {
-  const { cart, dispatch } = useShop()
-
-  const handleIncrementQuantity = (product: T_Product) => () => {
-    dispatch({
-      type: ShopActions.INCREMENT_ITEM_QUANTITY,
-      payload: product
-    })
-  }
-
-  const handleDecrementQuantity = (product: T_Product) => () => {
-    dispatch({
-      type: ShopActions.DECREMENT_ITEM_QUANTITY,
-      payload: product
-    })
-  }
+  const { cart } = useShop()
 
   return (
     <MainTemplate title="Carrinho">
-      <CartList
-        cart={cart}
-        handleDecrementQuantity={handleDecrementQuantity}
-        handleIncrementQuantity={handleIncrementQuantity}
-      />
+      <CartList cart={cart} />
 
       <TotalValueCard />
     </MainTemplate>
