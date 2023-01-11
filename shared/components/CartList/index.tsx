e0cf7d505c5344503e7f2ad069a7ec8c21b48747
ranoob <whyfck@gmail.com>
2023-01-11 15:54:03 -0300

    changes: moved handles to CartList

--- shared/components/CartList/index.tsx
+++ shared/components/CartList/index.tsx
@@ -1,28 +1,48 @@
 import styles from './styles.module.css'
 import { ProductCardOnCart } from '@/components'
+import { useShop } from '@/hooks'
+import { ShopActions } from '@/reducers'
 import { T_Product } from '@/types'
 
 const { container } = styles
 
 export type CartList = {
   cart: T_Product[]
-  handleDecrementQuantity: (product: T_Product) => () => void
-  handleIncrementQuantity: (product: T_Product) => () => void
 }
 
-export function CartList({
-  cart,
-  handleDecrementQuantity,
-  handleIncrementQuantity
-}: CartList) {
+export function CartList({ cart }: CartList) {
+  const { dispatch } = useShop()
+
+  const handleIncrementQuantity = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.INCREMENT_ITEM_QUANTITY,
+      payload: product
+    })
+  }
+
+  const handleDecrementQuantity = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.DECREMENT_ITEM_QUANTITY,
+      payload: product
+    })
+  }
+
+  const handleRemoveFromCart = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.REMOVE_FROM_CART,
+      payload: product
+    })
+  }
+
   return (
     <section className={container}>
       {cart.map((product) => (
         <ProductCardOnCart
-          product={product}
           key={product.name}
+          product={product}
           onDecrementQuantity={handleDecrementQuantity(product)}
           onIncrementQuantity={handleIncrementQuantity(product)}
+          onRemoveFromCart={handleRemoveFromCart(product)}
         />
       ))}
     </section>
