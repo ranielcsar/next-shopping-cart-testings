a17ba446284d12db375bebdee7bc8d9233050371
ranoob <whyfck@gmail.com>
2023-01-11 15:58:18 -0300

    feat: addItemToCart util

--- shared/reducers/shop-reducer/utils/index.tsx
+++ shared/reducers/shop-reducer/utils/index.tsx
@@ -1,3 +1,4 @@
 export { removeFromCart } from './removeFromCart'
 export { incrementItemQuantity } from './incrementItemQuantity'
 export { decrementItemQuantity } from './decrementItemQuantity'
+export { addItemToCart } from './addItemToCart'
