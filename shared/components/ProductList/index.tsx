22f0f9313b66724f467428c52f65bb02f417e1e9
ranoob <whyfck@gmail.com>
2023-01-11 15:56:06 -0300

    changes: moved handles

--- shared/components/ProductList/index.tsx
+++ shared/components/ProductList/index.tsx
@@ -1,15 +1,25 @@
 import styles from './styles.module.css'
 import { ProductCard } from '@/components'
+import { useShop } from '@/hooks'
+import { ShopActions } from '@/reducers'
 import { T_Product } from '@/types'
 
 const { container } = styles
 
 export type ProductListProps = {
   products: T_Product[]
-  handleOnAddToCart: (product: T_Product) => () => void
 }
 
-export function ProductList({ products, handleOnAddToCart }: ProductListProps) {
+export function ProductList({ products }: ProductListProps) {
+  const { dispatch } = useShop()
+
+  const handleOnAddToCart = (product: T_Product) => () => {
+    dispatch({
+      type: ShopActions.ADD_TO_CART,
+      payload: product
+    })
+  }
+
   return (
     <section className={container}>
       {products.map((product) => (
